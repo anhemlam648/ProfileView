f9de1cb187599931027abb564eaab9850ed768d4
VuTrungNghia <vunghia467@gmail.com>
2024-07-27 22:29:15 +0700

    UpdateJavascript

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import YouTube from 'react-youtube'; // Import react-youtube
-import './SpotifyPlayer.css';
 
 const SpotifyPlayer = () => {
   // Danh sách các bài hát
@@ -27,16 +26,55 @@ const SpotifyPlayer = () => {
       url: 'https://www.youtube.com/watch?v=cvoyUIIAdrY', 
     },
   ]);
+
+  const containerStyle = {
+    padding: '20px',
+  };
+
+  const listStyle = {
+    listStyleType: 'none',
+    padding: 0,
+  };
+
+  const songItemStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: '10px',
+    backgroundColor: '#f9f9f9',
+    padding: '10px',
+    borderRadius: '8px',
+  };
+
+  const imgStyle = {
+    width: '80px',
+    height: '80px',
+    borderRadius: '50%',
+    marginRight: '20px',
+  };
+
+  const songInfoStyle = {
+    flex: 1,
+  };
+
+  const songTitleStyle = {
+    fontWeight: 'bold',
+    color: '#888',
+  };
+
+  const songArtistStyle = {
+    fontStyle: 'italic',
+    color: '#888',
+  };
   return (
-    <div className="spotify-player">
+    <div style={containerStyle}>
       <h2 style={{ fontSize: '23px' }}>Spotify Playing 🎧</h2>
-      <ul>
+      <ul style={listStyle}>
         {songs.map((song) => (
-          <li key={song.id} className="song-item">
-            <img src={song.cover} alt="Song Cover" />
-            <div className="song-info">
-              <p className="song-title">{song.title}</p>
-              <p className="song-artist">{song.artist}</p>
+          <li key={song.id} style={songItemStyle}>
+            <img src={song.cover} alt="Song Cover" style={imgStyle} />
+            <div style={songInfoStyle}>
+              <p style={songTitleStyle}>{song.title}</p>
+              <p style={songArtistStyle}>{song.artist}</p>
             </div>
             {song.url.includes('youtube.com') && (
               <YouTube videoId={song.url.split('v=')[1]} opts={{ width: '100%', height: '100%' }} />
